60e5dc4dbda5e8ae768f73066614aae4668a9f69
Vladislav <prokhovich2@gmail.com>
2024-12-09 19:36:28 +0200

    change auth config

--- src/app/services/GoogleAuth/auth.config.ts
+++ src/app/services/GoogleAuth/auth.config.ts
@@ -2,8 +2,8 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
-  strictDiscoveryDocumentValidation: false,
-  clientId: '1045842654208-dcsa920tjltdo9maediv8mqdi19ceaj0.apps.googleusercontent.com',
-  redirectUri: window.location.origin+'/login/oauth2/code/google',
+
+  clientId: '831065904177-pfo1o5skuamubqvgr8ft7s3gua7je0fk.apps.googleusercontent.com',
+  redirectUri: "http://localhost:8080"+'/login/oauth2/code/google',
   scope: 'openid profile email',
 };
