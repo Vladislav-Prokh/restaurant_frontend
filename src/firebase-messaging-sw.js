bf44b91bbb2ee5a3c997cecd2039d5b2fd3f80e5
vladislav <prokhovich2@gmail.com>
2025-03-18 11:11:19 +0200

    replace secret data

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.6.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.6.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB3VZP62yoyK2gYLx5eFddQzUQUZCuATCY",
-  authDomain: "restaurant-4e9e0.firebaseapp.com",
-  projectId: "restaurant-4e9e0",
-  storageBucket: "restaurant-4e9e0.firebasestorage.app",
-  messagingSenderId: "551720016150",
-  appId: "1:551720016150:web:719d45cbd4e13497b31837",
-  measurementId: "G-0EC015STQW",
-  vapidKey: "BI3jqUl2dck4RXekI351Baf7tviGQk3nJLVmU-aGFYmPFVrt8NsNXBxPpbwzKjxyWk_I2i1lz5ZK8JULgoeNXYQ"
+  apiKey: "replace_it",
+  authDomain: "replace_it",
+  projectId: "replace_it",
+  storageBucket: "replace_it",
+  messagingSenderId: "replace_it",
+  appId: "replace_it",
+  measurementId: "replace_it",
+  vapidKey: "replace_it"
 });
 const messaging = firebase.messaging();
